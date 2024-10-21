b4f5b86a5503222df7a891fc02b996e3a1c8e2ea
Canib-us <162473942+Canib-us@users.noreply.github.com>
2024-10-21 16:40:59 +0700

    Add files via upload

---
+++ spring-beans-master/src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal{
+
+    public String toString(){
+        return "I'm a Dog";
+    }
+}
+
+
